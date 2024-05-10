fb56400f9df14894a26a596d74d92bce40bc56ca
Luisito Canlas <luisitocanlas@gmail.com>
2024-05-09 18:04:09 -0700

    Initial skeleton files

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(name, author, genre) {
+	this.name = name;
+	this.author = author;
+	this.genre = genre;
+}
+
+function addBookToLibrary() {}
+
+// loop through myLibrary array and display each book
+function browseLibrary() {}
